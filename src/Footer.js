8d973864dbcc45e088b24df6c31d8e5da779aeba
Issawaruk-Noothong <issawaruk.no.65@ubu.ac.th>
2023-06-20 13:41:04 +0700

    add message

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>Copyright © 2023 Anusorn Bunteong</span>
+        </div>       
+    );
+}
+
+export default Footer;
