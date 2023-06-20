ed3316ca7c0e3e0b54b2244adc7695040d726109
Issawaruk-Noothong <issawaruk.no.65@ubu.ac.th>
2023-06-20 19:36:59 +0700

    add message

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import { useState } from 'react';
 
 function App() {
   const [num, setNum] = useState(0);
+  let OddorEven = "ข้อความ";
+  var textno = " ไม่";
+  let IsnotEven = textno.bold();
+    if (num%2===0){
+      OddorEven = num + IsnotEven + "เป็นจำนวนเฉพาะ";
+    } else if(num%2!==0){
+      OddorEven = num + " เป็นจำนวนเฉพาะ";
+    }
+    document.getElementById("number").innerHTML = OddorEven;
   return (
     <div className="App">
       <Header />
       <div className="MyContent">
-        <h2>{num}</h2>
-        <button onClick={()=>setNum(num+1)} type="button" class="btn btn-primary">เพิ่ม</button>
-        <button type="button" class="btn btn-primary">Primary</button>
-        <button onClick={()=>setNum(num-1)} type="button" class="btn btn-primary">ลด</button>
+        <h2>{num}</h2><br></br>
+        <button className="btn btn-primary" onClick={()=>setNum(num+1)}>เพิ่ม</button>
+        &nbsp;&nbsp;
+        <button className="btn btn-primary" onClick={()=>setNum(num-1)}>ลด</button>
+        <br></br><br></br>
+        <span id="number"></span>
       </div>
       <Footer />
     </div>
